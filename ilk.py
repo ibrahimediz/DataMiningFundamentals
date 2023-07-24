28cee470213ddfea9d53b6646be59ae380bf82fc
İbrahim EDİZ <edizdesign@gmail.com>
2023-07-24 09:18:17 +0000

    24072023_2

--- ilk.py
+++ ilk.py
@@ -5,4 +5,9 @@ fileName = "ornek.ipynb"
 for item in liste:
     if not os.path.exists(os.path.join("Exercises",item)):
         os.mkdir(os.path.join("Exercises",item))
+    if not os.path.exists(os.path.join("Exercises",item,"data")):
+        os.mkdir(os.path.join("Exercises",item,"data"))
+    sourcePath = "/workspace/DataMiningFundamentals/Documents/01_Libraries/Data/heart.csv"
+    destPath = os.path.join("Exercises",item,"data","heart.csv")
+    shutil.copy(sourcePath,destPath)
     open(os.path.join("Exercises",item,fileName),"w+")
