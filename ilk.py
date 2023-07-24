1c33f272165377f77286a535ba30c11c09c8fa36
İbrahim EDİZ <edizdesign@gmail.com>
2023-07-24 11:26:33 +0000

    24072023_4

--- ilk.py
+++ ilk.py
@@ -1,13 +1,13 @@
 liste = ["Cevaplar","Serkan","Gül","Saniye","Sinan","Hatice","Tevfik","Mehmed","Şengül"]
 import shutil
 import os
-fileName = "ornek.ipynb"
+fileName = "ornek2.ipynb"
 for item in liste:
     if not os.path.exists(os.path.join("Exercises",item)):
         os.mkdir(os.path.join("Exercises",item))
     if not os.path.exists(os.path.join("Exercises",item,"data")):
         os.mkdir(os.path.join("Exercises",item,"data"))
-    sourcePath = "/workspace/DataMiningFundamentals/Documents/01_Libraries/Data/heart.csv"
-    destPath = os.path.join("Exercises",item,"data","heart.csv")
+    sourcePath = "/workspace/DataMiningFundamentals/Documents/01_Libraries/Data/50_Startups.csv"
+    destPath = os.path.join("Exercises",item,"data","50_Startups.csv")
     shutil.copy(sourcePath,destPath)
     open(os.path.join("Exercises",item,fileName),"w+")
